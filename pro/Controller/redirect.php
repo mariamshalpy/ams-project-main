e52d5d02417f52c0e9e8427b71736071739a15e5
Mariam Shalaby <98620529+mariamshalpy@users.noreply.github.com>
2023-05-07 21:37:10 +0300

    Add files via upload

---
+++ pro/Controller/redirect.php
@@ -0,0 +1,17 @@
+<?php 
+	include "db_conn.php";
+	session_start();
+	echo $_SESSION['role'];
+	if($_SESSION['role'] == 'admin'){
+		header("Location: ../Model/admin/home.php");
+	}else if($_SESSION['role'] == 'student'){
+		header("Location: ../View/student/home.php");
+	}else if($_SESSION['role'] == 'alumni'){
+		header("Location: ../View/alumni/home.php");
+	}else if($_SESSION['role'] == 'staff'){
+		header("Location: ../Model/staff/home.php");
+	}else if($_SESSION['role'] == 'support'){
+		header("Location: ../View/support/home.php");
+	}
+
+ ?>
