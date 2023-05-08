644b001205e676ed109d3119bb447a43e9a3f3f2
Mariam Shalaby <98620529+mariamshalpy@users.noreply.github.com>
2023-05-08 19:16:18 +0300

    Update stdevent.php

--- pro/Model/alumni&student/stdevent.php
+++ pro/Model/alumni&student/stdevent.php
@@ -5,10 +5,10 @@ class event_request{
     public function insertevent()
     {
         $use=new database;
-        if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['des'])&& isset($_POST['bdate']))
+        if(isset($_POST['name']) && isset($_POST['email']) )
   {
 
-    $data=array('Name '=>$_POST['name'],'Email'=>$_POST['email'],'Type'=>"participate",'Description' =>$_POST['des'],'date'=>$_POST['bdate']);
+    $data=array('Name '=>$_POST['name'],'Email'=>$_POST['email'],'Type'=>"participate");
           $use->insert('event_request',$data);
     }
 }
