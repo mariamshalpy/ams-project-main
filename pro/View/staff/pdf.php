dad4a33fc3a82adcf479a25e7f2ec56400aa5568
Mariam Saad <103897877+Mariam-Saad512003@users.noreply.github.com>
2023-05-09 12:33:58 +0300

    Update pdf.php

--- pro/View/staff/pdf.php
+++ pro/View/staff/pdf.php
@@ -2,10 +2,10 @@
 
 
 
-require_once "../../Model/survey.php";
+require_once "../../Model/staff/survey.php";
 require_once "../../Controller/staff/surveyHandler.php";
 require_once"Pdf/fpdf.php";
-
+$handler =new Surveyhandler();
 	
 ob_end_clean();
 
@@ -19,8 +19,14 @@ $pdf->AddPage();
 // Set the font for the text
 $pdf->SetFont('Arial', 'B', 18);
 
+$count=$handler->get_number();
 // Prints a cell with given text
-$pdf->Cell(60,20,'Hello GeeksforGeeks!');
+$pdf-> SetTitle("AMs",);
+$pdf->Cell(60,20,'Number of survey responce');
+$pdf->Cell(60,20,"");
+$pdf->Cell(60,20,$count);
+
+
 
 // return the generated output
 $pdf->Output();
