c6c420921c80d8eb2028e1b09207cc9353bd1d0a
MennaHefny25 <109662893+MennaHefny25@users.noreply.github.com>
2023-05-09 18:50:58 +0300

    Add files via upload

--- pro/Model/support.php
+++ pro/Model/support.php
@@ -2,15 +2,81 @@
 
 class Support{
 
-    public $ID;
-    public $FirstName;
-    public $LastName;
-    public $Email;
-    public $Address;
-    public $BirthDate;
-    public $Password;
-    public $PhoneNumber;
-    public $JobTitle;
+    private $ID;
+    private $FirstName;
+    private $LastName;
+    private $Email;
+    private $Address;
+    private $BirthDate;
+    private $Password;
+    private $PhoneNumber;
+    private $JobTitle;
+
+    public function get_first_name() {
+        return $this->FirstName;
+    }
+
+    public function get_last_name() {
+        return $this->LastName;
+    }
+
+    public function get_email() {
+        return $this->Email;
+    }
+
+    public function get_address() {
+        return $this->Address;
+    }
+
+    public function get_birth_date() {
+        return $this->BirthDate;
+    }
+
+    public function get_password() {
+        return $this->Password;
+    }
+
+    public function get_phone() {
+        return $this->PhoneNumber;
+    }
+
+    public function get_jobTitle() {
+        return $this->JobTitle;
+    }
+
+    // Setter methods
+    public function set_first_name($FirstName) {
+        $this->FirstName = $FirstName;
+    }
+
+    public function set_last_name($LastName) {
+        $this->LastName = $LastName;
+    }
+
+    public function set_email($Email) {
+        $this->Email = $Email;
+    }
+
+    public function set_address($Address) {
+        $this->Address = $Address;
+    }
+
+    public function set_birth_date($BirthDate) {
+        $this->BirthDate = $BirthDate;
+    }
+
+    public function set_password($Password) {
+        $this->Password = $Password;
+    }
+
+    public function set_phone($PhoneNumber) {
+        $this->PhoneNumber = $PhoneNumber;
+    }
+
+    public function set_job_title($JobTitle) {
+        $this->JobTitle = $JobTitle;
+    }
+
 
 }
 
